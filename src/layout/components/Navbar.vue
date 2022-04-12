852f88805cf59220ec7e7d382eb889275417b24e
程夏林 <10216491cheng_xialin@cn.tre-inc.com>
2022-04-12 13:41:00 +0800

    manager:面包屑行样式

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,36 +1,12 @@
 <template>
   <div class="navbar">
     <div class="hamburger-container">
-      <div>homepage</div>
-      <div v-for="(hamburger,i) in hamburgerList" :key="i">
-        / {{hamburger}}
-      </div>
-    </div>
-    <div class="right-menu">
-      <el-dropdown class="avatar-container" trigger="click">
-        <div class="avatar-wrapper">
-          <img :src="'http://172.17.100.50:8080/static/fe150e16/favicon.ico'" class="user-avatar">
-          <i class="el-icon-caret-bottom" />
-        </div>
-        <template #dropdown>
-          <el-dropdown-menu class="user-dropdown">
-            <router-link to="/">
-              <el-dropdown-item>
-                Home
-              </el-dropdown-item>
-            </router-link>
-            <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-              <el-dropdown-item>Github</el-dropdown-item>
-            </a>
-            <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-              <el-dropdown-item>Docs</el-dropdown-item>
-            </a>
-            <el-dropdown-item divided @click="logout">
-              <span style="display:block;">Log Out</span>
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
+      <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(hamburger,i) in hamburgerList" :key="i"
+      ><a href="/">{{hamburger}}</a></el-breadcrumb-item
+    >
+  </el-breadcrumb>
     </div>
   </div>
 </template>
@@ -56,11 +32,10 @@ export default {
 
 <style lang="less" scoped>
 .navbar {
-  height: 50px;
+  height: 40px;
   overflow: hidden;
   position: relative;
   background: #f2f3f5;
- 
 
   .hamburger-container {
     color: #8E8E8E;
@@ -69,69 +44,10 @@ export default {
     height: 100%;
     float: left;
     display: flex;
-    cursor: pointer;
     -webkit-tap-highlight-color:transparent;
-    &:hover {
-      background: rgba(0, 0, 0, .025)
-    }
-    & > div {
+    & > .el-breadcrumb {
       padding-left: 10px;
-    }
-  }
-
-  .breadcrumb-container {
-    float: left;
-  }
-
-  .right-menu {
-    float: right;
-    height: 100%;
-    line-height: 50px;
-
-    &:focus {
-      outline: none;
-    }
-
-    .right-menu-item {
-      display: inline-block;
-      padding: 0 8px;
-      height: 100%;
-      font-size: 18px;
-      color: #5a5e66;
-      vertical-align: text-bottom;
-
-      &.hover-effect {
-        cursor: pointer;
-        transition: background .3s;
-
-        &:hover {
-          background: rgba(0, 0, 0, .025)
-        }
-      }
-    }
-
-    .avatar-container {
-      margin-right: 30px;
-
-      .avatar-wrapper {
-        margin-top: 5px;
-        position: relative;
-
-        .user-avatar {
-          cursor: pointer;
-          width: 40px;
-          height: 40px;
-          border-radius: 10px;
-        }
-
-        .el-icon-caret-bottom {
-          cursor: pointer;
-          position: absolute;
-          right: -20px;
-          top: 25px;
-          font-size: 12px;
-        }
-      }
+      padding-top: 10px;
     }
   }
 }
