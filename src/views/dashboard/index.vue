5f6d89f26e51bb317da8eef427d73a85c498f911
程夏林 <10216491cheng_xialin@cn.tre-inc.com>
2022-05-18 09:02:19 +0800

    manager:仓库设置样式功能编写

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -70,6 +70,45 @@
       </div>
       </div>     
     </div>
+    <el-dialog
+  title="仓库设定"
+  v-model="dialogVisible"
+  width="900px"
+  :before-close="handleClose"
+>
+  <div>
+    <el-input v-model="input" placeholder="搜索仓库" size="large" style="width:418px; margin-bottom:12px ;" maxlength="100" @keyup.enter="getTableData" >
+    <template #suffix>
+          <svg v-show="input==''?false:true" @click="emptyInput" t="1649831312816" class="input-icon1" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2782" width="15" height="15"><path d="M512 32C251.4285715625 32 32 251.4285715625 32 512s219.4285715625 480 480 480 480-219.4285715625 480-480-219.4285715625-480-480-480z m205.7142853125 617.142856875c20.5714284375 20.5714284375 20.5714284375 48 0 61.714286249999994-20.5714284375 20.5714284375-48 20.5714284375-61.714285312499996 0l-137.142856875-137.1428578125L374.857143125 717.7142853125c-20.5714284375 20.5714284375-48 20.5714284375-68.5714284375 0s-20.5714284375-54.857143125 0-68.5714284375l144-144-137.1428578125-137.142856875c-20.5714284375-13.714285312500001-20.5714284375-41.142856875 0-61.714285312499996 20.5714284375-20.5714284375 48-20.5714284375 61.714286249999994 0l137.142856875 137.142856875 144-144c20.5714284375-20.5714284375 48-20.5714284375 68.5714284375 0 20.5714284375 20.5714284375 20.5714284375 48 0 68.5714284375L580.5714284375 512l137.142856875 137.142856875z" fill="#bfbfbf" p-id="2783"></path></svg>
+          <svg @click="selectGitLab" class="input-icon2" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-ba633cb8="" width="15" height="15"><path fill="currentColor" d="m795.904 750.72 124.992 124.928a32 32 0 0 1-45.248 45.248L750.656 795.904a416 416 0 1 1 45.248-45.248zM480 832a352 352 0 1 0 0-704 352 352 0 0 0 0 704z"></path></svg>
+        </template></el-input>
+    <div class="allwarehouse">
+      <span class="dialogTittle">全部仓库</span>
+      <div class="warehousebox1">
+        <div v-for="item in allselect" :key="item">
+          <el-checkbox v-model="checked1" :label="item" @change="checkSelect" style="margin-left:16px"></el-checkbox>
+        </div>
+          
+      </div>
+    </div>
+    <div class="selectedwarehouse">
+      <span class="dialogTittle">已选择</span>
+      <div class="warehousebox2">
+        <div v-for="(item,index) in selected" :key="index" style="margin-left: 8px;margin-right: 8px;">
+          <el-tag  closable type="info" style="width:100%; margin-bottom: 8px;height: 36px;font-size: 14px;" @close="handleClose(item)">
+    {{item}}
+  </el-tag>
+        </div>
+      </div>
+    </div>
+  </div>
+  <template #footer>
+    <span class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </span>
+  </template>
+</el-dialog>
   </div>
 </template>
 
@@ -82,18 +121,14 @@ export default {
       name: 'dashboard',
       username:'',
       usercd:'',
-      reviewDrawer:false,
-      synchronousDrawer:false,
-      branchValue:"",
-      addressValue:'',
-      addressinput:'',
-      userFlag:false,
+      checked1:[],
+      dialogVisible:true,
       pageTotal:50,
       curPage:1,
       pageSize:20,
-      pjUrl:'',
-      pjId:'',
       branchoptions:[],
+      allselect:['select1','select2','select3','select4','select5'],
+      selected:[],
       labs: [
         {
           name: '项目',
@@ -133,6 +168,13 @@ document.getElementsByClassName("el-pagination__total")[0].childNodes[0].nodeVal
     }
   },
   methods: {
+    handleClose(val){
+      this.selected.splice(this.selected.indexOf(val), 1)
+      console.log(this.selected);
+    },
+    checkSelect(){
+      this.selected=this.checked1
+    },
     getTitle(val){
       this.topTitle=val
     },
@@ -284,5 +326,40 @@ document.getElementsByClassName("el-pagination__total")[0].childNodes[0].nodeVal
   }
   .input-icon1{
     margin-right: 10px;
+  }
+  .allwarehouse,.selectedwarehouse{
+    width: 416px;
+    height: 450px;
+    border: 1px solid #EEEEEE;
+    border-radius: 5px;
+  }
+  .selectedwarehouse{
+    position:absolute;
+    top:136px;
+    left:460px;
+  }
+  .dialogTittle{
+    font-size: 16px;
+    color: black;
+    line-height: 40px;
+    height: 40px;
+    margin-left: 20px;
+    width: 100%;
+  }
+  .warehousebox1{
+    width: 100%;
+    border-top: 1px solid #EEEEEE;
+    padding-top: 6px;
+  }
+  .warehousebox2{
+    width: 100%;
+    border-top: 1px solid #EEEEEE;
+    padding-top: 16px;
+  }
+  .el-tag{
+    justify-content: left !important;
+  }
+  .el-tag .el-tag__close{
+    margin-left: 320px;
   }
 </style>
