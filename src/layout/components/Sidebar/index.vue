f22403ae4d46f2c1af09259a67b4df9f3340d65d
10105111liu_gaochao <10105111liu_gaochao@cn.tre-inc.com>
2022-06-22 17:12:16 +0800

    gitlab_UI:修改

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -12,10 +12,20 @@
         mode="vertical"
         router="true"
       >
-        <el-menu-item title="项目">
+        <el-menu-item title="项目" index="0">
           <svg-icon icon-class="home" />
           <template #title>项目功能稍后上线</template>
         </el-menu-item>
+        <li @click.stop="goUrl('https://code.trechina.cn/package')" class="el-menu-item" role="menuitem" tabindex="-1" title="资源包管理" style="padding-left: 20px;">
+          <div class="el-menu-tooltip__trigger el-tooltip__trigger el-tooltip__trigger">
+            <svg-icon icon-class="package" />
+          </div>
+        </li>
+        <li @click.stop="goUrl('https://forms.gle/RZB17dBePZ4GQ37t7')" class="el-menu-item" role="menuitem" tabindex="-1" title="虚拟机" style="padding-left: 20px;">
+          <div class="el-menu-tooltip__trigger el-tooltip__trigger el-tooltip__trigger">
+            <svg-icon icon-class="xuniji" />
+          </div>
+        </li>
         <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
       </el-menu>
     </el-scrollbar>
@@ -27,6 +37,12 @@ import SidebarItem from './SidebarItem.vue';
 
 export default {
   components: { SidebarItem },
+  methods: {
+    goUrl(url) {
+      console.log(123, 456);
+      window.location.href=url; 
+    }
+  },
   computed: {
     routes() {
       return this.$router.options.routes[1].children;
